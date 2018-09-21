6ef9416cae0f52c9bacc6f61aa66b7aeb7522e24
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2018-09-21 09:46:34 +0100

    media_jwplayer: Version bump.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018082400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018092100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700;        // Requires Moodle 3.5
 $plugin->component = 'media_jwplayer'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
